49033a928479fdf05c36375353509dc2bf620c4e
Ömer Faruk Demirsoy <omerfaruk.demirsoy@agu.edu.tr>
2023-02-12 00:37:35 +0300

    fixed project_model paragraph type

--- lib/models/project_model.dart
+++ lib/models/project_model.dart
@@ -1,18 +1,18 @@
-import 'dart:ui';
 
 import 'package:admin_client_portfolio/models/medias_model.dart';
+import 'package:admin_client_portfolio/models/paragraph_model.dart';
 
 class Project {
-  int id;
-  String projectName;
-  String projectType;
-  String projectTitle;
-  String introImg;
-  String projectIntro;
+  int? id;
+  String? projectName;
+  String? projectType;
+  String? projectTitle;
+  String? introImg;
+  String? projectIntro;
   List<Paragraph> paragraphs;
-  Medias medias;
-  List<String> members;
-  List<String> links;
+  Medias? medias;
+  List<String>? members;
+  List<String>? links;
 
   Project(
       {
@@ -29,16 +29,16 @@ class Project {
 });
 
   factory Project.fromJson(Map<String, dynamic> json) => Project(
-    id: json["id"] as int,
-    projectName: json["project_name"] as String,
-    projectType: json["project_type"] as String,
-    projectTitle: json["project_title"] as String,
-    introImg: json["intro_image"] as String,
-    projectIntro: json["project_intro"] as String,
+    id: json["id"] as int?,
+    projectName: json["project_name"] as String?,
+    projectType: json["project_type"] as String?,
+    projectTitle: json["project_title"] as String?,
+    introImg: json["intro_image"] as String?,
+    projectIntro: json["project_intro"] as String?,
     paragraphs: json["paragraphs"] as List<Paragraph>,
-    medias: json["medias"] as Medias,
-    members: json["members"] as List<String>,
-    links: json["links"] as List<String>,
+    medias: json["medias"] as Medias?,
+    members: json["members"] as List<String>?,
+    links: json["links"] as List<String>?,
   );
 
   Map<String, dynamic> toJson() => {
