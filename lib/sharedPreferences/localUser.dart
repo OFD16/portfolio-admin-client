16c08dc6c61b91734c83efc281f9c508bd5bd0cd
Ömer Faruk Demirsoy <omerfaruk.demirsoy@agu.edu.tr>
2023-02-16 03:24:13 +0300

    localUserData saving success

--- lib/sharedPreferences/localUser.dart
+++ lib/sharedPreferences/localUser.dart
@@ -9,19 +9,21 @@ class LocalUserData {
 
   setLocalUser(User newUser) async {
     String jsonUser = json.encode(newUser.toJson());
-
+    print('newUser: ${newUser.id}');
+    States().setLocalUser(newUser);
+    print('stateUser: ${States().localUserData.id}');
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     sharedPreferences.setString(_localUserDataKey, jsonUser);
   }
 
-  Future<User> getLocalUser() async {
+   getLocalUser() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     String? jsonUser = sharedPreferences.getString(_localUserDataKey);
 
     if (jsonUser != null) {
       Map<String, dynamic> userMap = json.decode(jsonUser);
       User user = User.fromJson(userMap);
-      States().localUserData = user;
+      print('user: ${user.id}');
       return user;
     } else {
       return User(id: 0, firstName: "", lastName: "", age: 0, email: "", userImg: "", introduction: "", markedProjects: [], markedBlogs: [], role: "");
