b1bcf3f516b39b53aa27d633ceda588b9a3ce476
Ömer Faruk Demirsoy <omerfaruk.demirsoy@agu.edu.tr>
2023-02-18 06:45:58 +0300

    fixed initial size and icon

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -25,9 +25,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
+  Win32Window::Point origin(400, 200);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"admin_client_portfolio", origin, size)) {
+  if (!window.Create(L"Seni Seviyorum", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
